d7249f60597f87f1de4ea6d586fe0c1d4ffb4f66
sp-merrow <87612795+sp-merrow@users.noreply.github.com>
2021-08-12 17:48:05 -0400

    game now fully playable

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -18,33 +18,32 @@ boardImage = pygame.transform.scale(pygame.image.load('board.png'), (500, 500))
 xImg = pygame.transform.scale(pygame.image.load('x.png'), ( round(screenWidth/3), round(screenWidth/3) ) )
 oImg = pygame.transform.scale(pygame.image.load('o.png'), ( round(screenWidth/3), round(screenWidth/3) ) )
 baseFont = pygame.font.Font(None, 32)
-
-allZones = pygame.sprite.Group()
+clickZones = []
 
 class Zone(pygame.sprite.Sprite):
     def __init__(self, visualObj, coords):
         self.visualObj = visualObj
         self.coords = coords
         self.rect = self.visualObj.get_rect()
-        pygame.sprite.Sprite.__init__(self)
+        self.rect.topleft = coords
 
-clickZones = []
-x, y = 0, 0
-for i in range(9):
-    sizeFract = 500/3
-    if i == 0:
-        pass
-    elif i % 3 == 0:
-        y += sizeFract
-        x = 0
-    else:
-        x += sizeFract
-    newSurface = pygame.Surface( (sizeFract, sizeFract) )
-    newSurface.fill((255, 255, 255))
-    newZone = Zone(newSurface, (round(x), round(y)))
-    clickZones.append(newZone)
-
-allZones.add(clickZones)
+def newClickZones():
+    global clickZones
+    clickZones = []
+    x, y = 0, 0
+    for i in range(9):
+        sizeFract = 500/3
+        if i == 0:
+            pass
+        elif i % 3 == 0:
+            y += sizeFract
+            x = 0
+        else:
+            x += sizeFract
+        newSurface = pygame.Surface( (sizeFract, sizeFract) )
+        newSurface.fill((255, 255, 255))
+        newZone = Zone(newSurface, (round(x), round(y)))
+        clickZones.append(newZone)
 
 running = True
 players = []
@@ -58,6 +57,31 @@ inactiveColor = (100, 100, 100)
 activeColor = (202, 202, 202)
 color = inactiveColor
 active = False
+newClickZones()
+
+def hasWon(locations):
+    for i in locations:
+        if i in {0, 3, 6}:
+            if all(x in locations for x in range(i, i+3)):
+                return True
+        if i in {0, 1, 2}:
+            if all(x in locations for x in range(i, i+7, 3)):
+                return True
+        if i == 0:
+            if all(x in locations for x in range(0, 9, 4)):
+                return True
+        if i == 2:
+            if all(x in locations for x in range(2, 7, 2)):
+                return True
+    return False
+
+def getWinner():
+    if hasWon(xLocs):
+        return f'{players[0]} wins!'
+    if hasWon(oLocs):
+        return f'{players[1]} wins!'
+    return None
+
 pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 while running:
     for event in pygame.event.get():
@@ -91,7 +115,7 @@ while running:
 
     screen.fill((255, 255, 255))
     inputSurf = baseFont.render(inputText, True, (0, 0, 0))
-    screen.blit(inputSurf, (inputRect.x+5, inputRect.y+5))
+    screen.blit(inputSurf, (inputRect.x+5, (inputRect.centery - (inputSurf.get_rect().h/2))))
     pygame.draw.rect(screen, color, inputRect, 2)
     screen.blit(text, textRect)
 
@@ -102,79 +126,90 @@ while running:
 
 
 running = True
-xLocs = []
-oLocs = []
+xLocs, oLocs = [], []
 count = 1
 
-def hasWon(locations):
-    for i in locations:
-        if i in {0, 3, 6}:
-            if all(x in locations for x in range(i, i+3)):
-                return True
-        if i in {0, 1, 2}:
-            if all(x in locations for x in range(i, i+7, 3)):
-                return True
-        if i == 0:
-            if all(x in locations for x in range(0, 9, 4)):
-                return True
-        if i == 2:
-            if all(x in locations for x in range(2, 7, 2)):
-                return True
-    return False
-
-def anyWon():
-    if hasWon(xLocs):
-        return f'{players[0]} wins!'
-    if hasWon(oLocs):
-        return f'{players[1]} wins!'
-    return False
-
-collisionTesters = {}
-for c, i in enumerate(clickZones):
-    collisionTesters[c] = pygame.Rect(i.coords[0], i.coords[1], i.visualObj.get_width(), i.visualObj.get_height())
-
-pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+continueText = baseFont.render('Play again', True, (0, 0, 0), None)
+continueButton = pygame.Rect((screenWidth/2), (screenHeight/2), (screenWidth/1.5), 40)
+continueButton.center = (screenWidth/2, 400)
+continueCoords = ( (continueButton.centerx - (continueText.get_rect().w/2)), ( continueButton.centery - ( continueText.get_rect().h/2 ) ) )
+exitText = baseFont.render('Exit', True, (0, 0, 0), None)
+exitButton = pygame.Rect((screenWidth/2), (screenHeight/2), (screenWidth/1.5), 40)
+exitButton.center = (screenWidth/2, 500)
+exitCoords = ( (exitButton.centerx - (exitText.get_rect().w/2)), (exitButton.centery - (exitText.get_rect().h/2)) )
+buttonsPlaced = False
 
 while running:
     clickedBox = []
     for event in pygame.event.get():
+        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+        if continueButton.collidepoint(pygame.mouse.get_pos()) and buttonsPlaced:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+            continueColor = (150, 150, 150)
+            exitColor = (0, 0, 0)
+        elif exitButton.collidepoint(pygame.mouse.get_pos()) and buttonsPlaced:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+            exitColor = (150, 150, 150)
+            continueColor = (0, 0, 0)
+        else:
+            exitColor = continueColor = (0, 0, 0)
         if (event.type == KEYDOWN and event.key == K_ESCAPE) or event.type == QUIT:
             pygame.quit()
-        if event.type == MOUSEBUTTONUP:
-            for c, i in enumerate(clickZones):
-                if collisionTesters[c].collidepoint(event.pos) and i[0] not in {xImg, oImg}:
-                    if count % 2 == 0:
-                        clickedBox.append(c)
-                        clickedBox.append(oImg)
-                        oLocs.append(c)
-                        count += 1
-                    else:
-                        clickedBox.append(c)
-                        clickedBox.append(xImg)
-                        xLocs.append(c)
-                        count += 1
-        if allZones.collidepoint(pygame.mouse.get_pos()): #///////////////////////////////////////////////// LEFT OFF HERE
-            print('collision detected')
+        for c, i in enumerate(clickZones):
+            if event.type == MOUSEBUTTONUP and i.rect.collidepoint(event.pos) and i.visualObj not in {xImg, oImg}:
+                if count % 2 == 0:
+                    clickedBox.append(c)
+                    clickedBox.append(oImg)
+                    oLocs.append(c)
+                    count += 1
+                else:
+                    clickedBox.append(c)
+                    clickedBox.append(xImg)
+                    xLocs.append(c)
+                    count += 1
+            if i.rect.collidepoint(pygame.mouse.get_pos()) and i.visualObj not in {xImg, oImg}:
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+        if event.type == MOUSEBUTTONUP and buttonsPlaced:
+            if exitButton.collidepoint(event.pos):
+                pygame.quit()
+            if continueButton.collidepoint(event.pos):
+                count = 1
+                newClickZones()
+                xLocs, oLocs = [], []
 
-    if count % 2 == 0:
-        text = baseFont.render(f"{players[1]}'s turn", True, (0, 0, 0), None)
+
+    if count < 10 and not getWinner():
+        buttonsPlaced = False
+        if count % 2 == 0:
+            text = baseFont.render(f"{players[1]}'s turn", True, (0, 0, 0), None)
+        else:
+            text = baseFont.render(f"{players[0]}'s turn", True, (0, 0, 0), None)
+        textRect = text.get_rect()
+        textRect.center = (screenWidth/2, 550)
+
+        if clickedBox:
+            clickZones[clickedBox[0]].visualObj = clickedBox[1]
+        screen.fill((255, 255, 255))
+        for s in clickZones:
+            screen.blit(s.visualObj, s.coords)
+        screen.blit(boardImage, (0,0))
+        screen.blit(text, textRect)
+        
+
     else:
-        text = baseFont.render(f"{players[0]}'s turn", True, (0, 0, 0), None)
-    textRect = text.get_rect()
-    textRect.center = (screenWidth/2, 550)
-
-    if clickedBox:
-        clickZones[clickedBox[0]].visualObj = clickedBox[1]
-    screen.fill((255, 255, 255))
-    for s in clickZones:
-        screen.blit(s.visualObj, s.coords) 
-    screen.blit(boardImage, (0,0))
-    screen.blit(text, textRect)
-
+        if count == 10:
+            endTxt = 'CATS GAME!'
+        else:
+            endTxt = getWinner()
+        text = baseFont.render(f"{endTxt}", True, (0, 0, 0), None)
+        textRect = text.get_rect()
+        textRect.center = (screenWidth/2, 200)
+        screen.fill((255, 255, 255))
+        screen.blit(text, textRect)
+        screen.blit(continueText, continueCoords)
+        pygame.draw.rect(screen, continueColor, continueButton, 2)
+        screen.blit(exitText, exitCoords)
+        pygame.draw.rect(screen, exitColor, exitButton, 2)
+        buttonsPlaced = True
+    
     pygame.display.flip()
-    if count == 10:
-        running = False
-    if anyWon():
-        running = False
-
-pygame.quit()
